4316d58b7ac590d8f901d0853ec0efb9344deb54
TuanHung1995 <tuanhungg1995@gmail.com>
2025-02-25 15:11:17 +0700

    show message for admin

--- app/Http/Controllers/Backend/MessageController.php
+++ app/Http/Controllers/Backend/MessageController.php
@@ -8,7 +8,8 @@ use Illuminate\Http\Request;
 
 class MessageController extends Controller
 {
-    function index() {
+    function index()
+    {
         $userId = auth()->user()->id;
 
         $chatUsers = Chat::with('senderProfile')->select(['sender_id'])
@@ -17,5 +18,19 @@ class MessageController extends Controller
             ->groupBy('sender_id')
             ->get();
         return view('admin.messenger.index', compact('chatUsers'));
+    }
+
+    function getMessages(Request $request)
+    {
+        $senderId = auth()->user()->id;
+        $receiverId = $request->receiver_id;
+
+        $messages = Chat::whereIn('receiver_id', [$senderId, $receiverId])
+            ->whereIn('sender_id', [$senderId, $receiverId])
+            ->orderBy('created_at', 'asc')
+            ->get();
+        Chat::where(['sender_id' => $receiverId, 'receiver_id' => $senderId])->update(['seen' => 1]);
+
+        return response($messages);
     }
 }
--- resources/views/admin/layouts/master.blade.php
+++ resources/views/admin/layouts/master.blade.php
@@ -43,6 +43,14 @@
 
         gtag('config', 'UA-94034622-3');
     </script>
+
+    <script>
+        const USER = {
+            id: "{{ auth()->user()->id }}",
+            name: "{{ auth()->user()->name }}",
+            image: "{{ asset(auth()->user()->image) }}",
+        }
+    </script>
     <!-- /END GA -->
 </head>
 
@@ -58,16 +66,6 @@
             <div class="main-content">
                 @yield('content')
             </div>
-
-            <footer class="main-footer">
-                <div class="footer-left">
-                    Copyright &copy; 2018 <div class="bullet"></div> Design By <a href="https://nauval.in/">Muhamad
-                        Nauval Azhar</a>
-                </div>
-                <div class="footer-right">
-
-                </div>
-            </footer>
         </div>
     </div>
 
